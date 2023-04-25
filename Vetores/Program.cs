1b45f97571250e809ef5440959938a45f6c5b98d
David Ramos <david.ramos@aedb.br>
2023-04-25 16:50:07 -0300

    Up

---
+++ Vetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // variável que recebe o numero de repetição
+
+            Produto[] vet = new Produto[n]; // Criação do vetor de nome "vet"
+
+            for (int i = 0; i < n; i++) // FOR para preencher 
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine());
+                vet[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++) // FOR para somar o campo soma do objeto vet.
+            {
+                soma += vet[i].Preco;
+            }
+
+            double med = soma / n;
+            Console.WriteLine("Media: " + med);
+
+
+        }
+
+    }
+}
