7552744f6b604dd09fe5c3c9fd3e9449c2662860
David Ramos <david.ramos@aedb.br>
2023-05-04 11:22:12 -0300

    Fechamanto cap 6.

---
+++ Matriz/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Matriz // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int l = 0; l < n; l++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+
+                for (int c = 0; c < n; c++)
+                {
+                    mat[l, c] = int.Parse(vet[c]);
+                }                
+
+            }
+
+            Console.WriteLine("Diagonal principal: ");
+            for (int l = 0; l < n; l++)
+            {
+                Console.Write(mat[l, l] + " ");
+
+            }
+
+            int Count = 0; 
+            for (int l = 0; l < n; l++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (mat[l, c] < 0)
+                    {
+                        Count++;
+                    }
+                    
+                }
+
+            }
+            Console.WriteLine();
+            Console.Write( "Negativos: " + Count);
+
+
+        }
+    }
+}
---
+++ Matriz_Exe1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Matrtiz_Exe1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write ("Tamanho da matiz: ");
+            int ln = int.Parse(Console.ReadLine());
+            int cl = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[ln, cl];
+
+            for (int l = 0; l < ln; l++)
+            {
+                string [] vet = Console.ReadLine().Split (' ');   // Cria um vetor de armazenamento temporário dos dados             
+
+                for (int c = 0; c < cl; c++)
+                {
+                    mat[l, c] = int.Parse(vet[c]); // Converte a string do vetor "vet" em int e coloca na posição "c" da linha e coluna
+                }
+            }
+
+
+        }
+    }
+}
